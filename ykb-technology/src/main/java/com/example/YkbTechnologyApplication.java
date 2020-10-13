2f9225a1dbc42bd58933cea0421edbaa0b235858
ykb <769505129@qq.com>
2020-10-13 17:24:16 +0800

    第一次提交

---
+++ ykb-technology/src/main/java/com/example/YkbTechnologyApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YkbTechnologyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YkbTechnologyApplication.class, args);
+    }
+
+}
