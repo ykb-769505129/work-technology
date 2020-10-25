8f8016af10dcf84958134b255a0cf41c54194c3a
wcq <624801921@qq.com>
2020-10-25 12:37:55 +0800

    1

---
+++ nacos-discovery/quickstart-provider/src/main/java/com/wcq/controller/ProviderController.java
@@ -0,0 +1,17 @@
+package com.wcq.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProviderController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ProviderController.class);
+    @GetMapping("/service")
+    public String service(){
+        LOG.info("provider invoke");
+        return "provider invoke";
+    }
+}
---
+++ nacos-discovery/quickstart-provider/src/main/java/com/wcq/quickstartprovider/Application.java
@@ -0,0 +1,15 @@
+package com.wcq.quickstartprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication(scanBasePackages = "com.wcq")
+@EnableDiscoveryClient
+@EnableFeignClients
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
